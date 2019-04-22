a082666486aa13c4aaf1043b7dc53a124135d46b
Kyle <39305051+KyleBarrett@users.noreply.github.com>
2019-04-22 05:38:56 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 
-pkg <- c("shiny", "shinyAce", "shinydashboard", "dplyr", "knitr", "plyr", "tidyverse", "cowplot"
+pkg <- c("shiny", "shinyAce", "shinydashboard", "dplyr", "knitr", "plyr", "tidyverse", "cowplot",
          "wrapr", "extrafont", "polynom", "ggplot2", "shinyWidgets", "gridExtra", "rmarkdown",
          "markdown", "sn", "rlang", "lattice", "reshape", "reshape2", "magrittr", "stats","reactlog","devtools")
 new.pkg <- pkg[!(pkg %in% installed.packages())]
